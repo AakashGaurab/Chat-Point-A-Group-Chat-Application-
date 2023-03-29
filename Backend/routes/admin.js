63f617b9dcce10796b788c4a8d1528ccebde69db
Aakash <aakashgaurav456@gmail.com>
2023-03-29 15:40:15 +0530

    Admin backend

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -3,6 +3,25 @@ const express = require("express");
 const admin = express.Router();
 
 
+app.get("/read",(req,res)=>{
+
+})
+
+
+app.post("/create",(req,res)=>{
+
+})
+
+
+app.put("/update",(req,res)=>{
+    
+})
+
+
+app.delete("/delete",(req,res)=>{
+
+})
+
 
 
 
