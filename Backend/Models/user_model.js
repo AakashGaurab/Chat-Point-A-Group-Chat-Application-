4d8d13cf4988139fcecf533958e8586ab5a8e88f
unknown <smartdesk2015@gmail.com>
2023-03-30 17:41:03 +0530

    parimal

--- Backend/Models/user_model.js
+++ Backend/Models/user_model.js
@@ -3,13 +3,11 @@ const mongoose = require("mongoose");
 const UserSchema = mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    unique: true,
+    required: true
   },
   email: {
     type: String,
     required: true,
-    unique: true,
   },
   password: {
     type: String,
@@ -26,3 +24,4 @@ const UserSchema = mongoose.Schema({
 const UserModel = mongoose.model("users", UserSchema);
 
 module.exports = UserModel;
+
